d1ea9368d23afab3f088124d4666986fdbebfdf2
Your <yosephtatek88@gamil.com>
2023-09-11 15:15:25 -0700

    loops

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+  *main - main entry point
+  *
+  *Discription: 'smallest combination of three numbers in sequence'
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (i < j)
+			{
+				for (k = 48; k <= 57; k++)
+				{
+					if (j < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						putchar(',');
+						putchar(' ');
+					}
+					else
+					{
+						continue;
+					}
+				}
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
