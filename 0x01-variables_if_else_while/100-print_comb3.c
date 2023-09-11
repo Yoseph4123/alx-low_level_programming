ff01c87abe301d34d06f474d5e7fb67cb4e89a9c
Your <yosephtatek88@gamil.com>
2023-09-11 14:53:36 -0700

    loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  *main - main entry point
+  *
+  *Description: 'smallest combination of two no'
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <=57; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			else 
+			{
+				continue;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
