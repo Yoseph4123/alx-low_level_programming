608529ae45e94058ea9f460c6dca313d4208eb6e
Your <yosephtatek88@gamil.com>
2023-09-11 10:00:34 -0700

    unox

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 
-int main(void) {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
-    return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59;
+
+    write(2, str, len);
+    return (1);
 }
