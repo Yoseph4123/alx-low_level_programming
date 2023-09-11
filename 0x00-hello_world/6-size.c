d1a1ebc27081d02d1b26eaef261f608976a92065
Your <yosephtatek88@gamil.com>
2023-09-11 09:21:46 -0700

    c language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
   *main - main entery point
-  *return: always 0 (sucess)
+  *
+  *Return: always 0 (sucess)
   */
 int main(void)
 {
@@ -9,6 +10,6 @@ int main(void)
 	printf("Size of an int: %lu bytes\n", sizeof(int));
 	printf("Size of an long  int: %lu bytes\n", sizeof(long int));
 	printf("Size of an long long int: %lu bytes\n", sizeof(long long int));
-	printf("Size of an float: %lu bytes\n", sizeof(float));
+	printf("Size of an float: %lu bytes", sizeof(float));
 	return (0);
 }
