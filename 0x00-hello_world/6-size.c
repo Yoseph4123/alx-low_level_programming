df6717e805420a7bd389a16ccb1496b129e99783
Your <yosephtatek88@gamil.com>
2023-09-11 08:52:49 -0700

    c language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+  *main - main entery point
+  *return: always 0 (sucess)
+  */
+int main(void)
+{
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of an long  int: %lu bytes\n", sizeof(long int));
+	printf("Size of an long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of an float: %lu bytes\n", sizeof(float));
+	return (0);
+}
