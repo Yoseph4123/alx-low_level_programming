06f6272918c8fd5e76132e1e498cd4185f4d33b9
Your <yosephtatek88@gamil.com>
2023-09-13 16:18:20 -0700

    function nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,24 +13,26 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if (i == 0 || j == 0)
-			{
-				_putchar('0');
-			}
 
-			if (k < 10 && j != 0)
+			if ((k / 10) == 0)
 			{
-				_putchar(44);
-				_putchar(' ');
-				_putchar(' ');
+				if (j != 0)
+					_putchar(' ');
 				_putchar(k + 48);
-			}
-			else if (k >= 10)
-			{
-				_putchar(44);
+
+				if (j == 9)
+					continue;
+				_putchar(',');
 				_putchar(' ');
+			}
+			else
+			{
 				_putchar((k / 10) + 48);
 				_putchar((k % 10) + 48);
+				if (j == 9)
+					continue;
+				_putchar(44);
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
