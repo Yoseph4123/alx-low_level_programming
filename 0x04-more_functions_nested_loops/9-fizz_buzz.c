17912c56011295d9cc78b74d9a9fc58b79309b8e
Your <yosephtatek88@gamil.com>
2023-09-15 16:16:03 -0700

    more functions and nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
-		else if (i == 100)
+		else if (i == 1)
 		{
-			printf("FizzBuzz");
+			printf("%d", i);
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
