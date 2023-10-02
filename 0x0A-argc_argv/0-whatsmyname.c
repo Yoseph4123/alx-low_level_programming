ed2b12b9bda8e1efaef466e8c6606757a325cbb9
Your <yosephtatek88@gamil.com>
2023-10-02 12:33:37 -0700

    avgc avgv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
-int main(int avgc,char* avgv[])
+/**
+  *main - entry pointer
+  *@avgc: is an integer
+  *@avgv: is a string
+  *Return: always 0 (sucess)
+  */
+int main(int avgc, char *avgv[])
 {
 	int i;
-	for (i = 0;i < avgc;i++)
+
+	for (i = 0; i < avgc; i++)
 	{
 		printf("%s\n", avgv[i]);
 	}
-	return 0;
+	return (0);
 }
